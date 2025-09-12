4ace75dbbec95caaa027fe4eff520d2062c6a431
Dawson K. <46154365+TheInfinityIQ@users.noreply.github.com>
2025-09-12 13:52:55 -0500

    Navigation tiles removed when obstacles placed

--- scripts/terrain.gd
+++ scripts/terrain.gd
@@ -1,23 +1,11 @@
 extends TileMapLayer
 
-@onready var _obstacles: TileMapLayer = $"../obstacles"
-@onready var _bridges: TileMapLayer = $"../Bridges"
-
-const TERRAIN_TILESET_ID = 3
-const OBSTACLES_TILESET_ID = 6
-const BRIDGES_TILESET_ID = 1
+@onready var _obstacles: TileMapLayer = $"../Obstacles"
 
 func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
-	if coords in _obstacles.get_used_cells_by_id(OBSTACLES_TILESET_ID):
+	if _obstacles.get_cell_tile_data(coords):
 		return true
 	return false
 
 func _tile_data_runtime_update(coords: Vector2i, tile_data: TileData) -> void:
-	if coords in _obstacles.get_used_cells_by_id(OBSTACLES_TILESET_ID):
-		print("here")
-		tile_data.set_navigation_polygon(TERRAIN_TILESET_ID, null)
-	
-	#if coords in _bridges.get_used_cells_by_id(BRIDGES_TILESET_ID):
-		#var nav_polygon: NavigationPolygon = NavigationPolygon.new()
-		#nav_polygon.
-		#tile_data.set_navigation_polygon(BRIDGES_TILESET_ID, nav_polygon)
+	tile_data.set_navigation_polygon(0, null)
