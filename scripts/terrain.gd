b6e91d2c769372926e06720b4462b323eb6b4c84
Dawson K. <46154365+TheInfinityIQ@users.noreply.github.com>
2025-09-12 13:27:15 -0500

    Corrected typo

--- scripts/terrain.gd
+++ scripts/terrain.gd
@@ -1,19 +1,19 @@
 extends TileMapLayer
 
-@onready var _obsticles: TileMapLayer = $"../Obsticles"
+@onready var _obstacles: TileMapLayer = $"../obstacles"
 @onready var _bridges: TileMapLayer = $"../Bridges"
 
 const TERRAIN_TILESET_ID = 3
-const OBSTICLES_TILESET_ID = 6
+const OBSTACLES_TILESET_ID = 6
 const BRIDGES_TILESET_ID = 1
 
 func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
-	if coords in _obsticles.get_used_cells_by_id(OBSTICLES_TILESET_ID):
+	if coords in _obstacles.get_used_cells_by_id(OBSTACLES_TILESET_ID):
 		return true
 	return false
 
 func _tile_data_runtime_update(coords: Vector2i, tile_data: TileData) -> void:
-	if coords in _obsticles.get_used_cells_by_id(OBSTICLES_TILESET_ID):
+	if coords in _obstacles.get_used_cells_by_id(OBSTACLES_TILESET_ID):
 		print("here")
 		tile_data.set_navigation_polygon(TERRAIN_TILESET_ID, null)
 	
